1fd5cfe549aa980b56ec4b954fea85c50579d9e7
Nadhif <nadhifalrozin@gmail.com>
2024-03-31 02:13:14 +0700

    update database, adding user table and it function

--- models/database.js
+++ models/database.js
@@ -10,24 +10,127 @@ const pool = mysql.createPool({
     database: process.env.MYSQL_DATABASE
 }).promise()
 
+// Makanan
+
+var blankMakanan = 
+[{
+    "id": "Nothing Found",
+    "foodName": "Nothing Found",
+    "foodOrDrink": "Nothing Found",
+    "images": "Nothing Found",
+    "rating": "Nothing Found"
+}]
+
 async function getMakananById(a){
-    const result = await pool.query(`
+    var result = await pool.query(`
     SELECT * FROM kulinerNangor
     WHERE id = ?
     `, [a])
-    return result[0][0]['foodName']
+    if (result[0].length == 0){
+        return blankUser[0]
+    }
+    return result[0][0]
 }
 
-async function getImageById(a){
+async function getMakananByName(a){
     const result = await pool.query(`
     SELECT * FROM kulinerNangor
-    WHERE id = ?
+    WHERE foodName = ?
     `, [a])
-    return result[0][0]['images']
+    return result[0]
 }
 
+async function getMakananByRating(a){
+    var result = await pool.query(`
+    SELECT * FROM kulinerNangor
+    WHERE rating = ?
+    `, [a])
+    if (result[0].length == 0){
+        return blankMakanan
+    }
+    return result[0]
+}
+
+async function getMakananByType(a){
+    const result = await pool.query(`
+    SELECT * FROM kulinerNangor
+    WHERE foodOrDrink = ?
+    `, [a])
+    return result[0]
+}
+
+// User
+
+var blankUser = 
+[{
+    "uid": "Nothing Found",
+    "userName": "Nothing Found",
+    "profilePicture": "Nothing Found",
+    "userAge": "Nothing Found",
+    "gender": "Nothing Found",
+    "countFood": "Nothing Found"
+}]
+
+async function getUserByUid(a){
+    var result = await pool.query(`
+    SELECT * FROM dataUser
+    WHERE uid = ?
+    `, [a])
+    if (result[0].length == 0){
+        return blankUser[0]
+    }
+    return result[0][0]
+}
+
+async function getUserByName(a){
+    const result = await pool.query(`
+    SELECT * FROM dataUser
+    WHERE userName = ?
+    `, [a])
+    return result[0]
+}
+
+async function getUserByCount(a){
+    var result = await pool.query(`
+    SELECT * FROM dataUser
+    WHERE countFood = ?
+    `, [a])
+    if (result[0].length == 0){
+        return blankUser
+    }
+    return result[0]
+}
+
+async function getUserLessByCount(a){
+    var result = await pool.query(`
+    SELECT * FROM dataUser
+    WHERE countFood <= ?
+    `, [a])
+    if (result[0].length == 0){
+        return blankUser
+    }
+    return result[0]
+}
+
+async function getUserMoreByCount(a){
+    var result = await pool.query(`
+    SELECT * FROM dataUser
+    WHERE countFood >= ?
+    `, [a])
+    if (result[0].length == 0){
+        return blankUser
+    }
+    return result[0]
+}
 
 module.exports = {
     getMakananById,
-    getImageById
+    getMakananByName,
+    getMakananByRating,
+    getMakananByType,
+    getUserByUid,
+    getUserByName,
+    getUserByCount,
+    getUserLessByCount,
+    getUserMoreByCount,
 }
--- routes/home.js
+++ routes/home.js
@@ -3,19 +3,21 @@ const router = express.Router()
 const db = require('../models/database')
 
 router
-    .get('/1',async (req, res) => {
-        res.render('home/index')
-    })
     .get('/',async (req, res) => {
         res.render('home/index')
     })
-    .get('/1',async (req, res) => {
-        hasil = await db.getMakananById(1)
-        res.render('home/index', {data: hasil})
+    .get('/test',async (req, res) => {
+        let gambar = (await db.getMakananById(1, 0))["images"]
+        let nama = (await db.getMakananByRating(10))[0]["foodName"]
+        let userName = (await db.getUserByUid(1))["userName"]
+        let age = (await db.getUserByCount(-5))[0]["userAge"]
+        
+        const Hasil = [gambar, nama, userName, age]
+        res.render('home/test', {data: Hasil})
     })
     .get('/img',async (req, res) => {
-        linkGambar = await db.getImageById(1)
-        res.render('home/index', {data: linkGambar})
+        linkGambar = (await db.getMakananById(1, 0))["images"]
+        res.render('home/information', {data: linkGambar})
     })
 
 module.exports = router
