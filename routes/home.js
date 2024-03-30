18ce0280bd8400c9d1469a55bfe158893874561b
Carlo Angkisan <130674488+carllix@users.noreply.github.com>
2024-03-30 22:06:51 +0700

    test

--- routes/home.js
+++ routes/home.js
@@ -3,6 +3,9 @@ const router = express.Router()
 const db = require('../models/database')
 
 router
+    .get('/1',async (req, res) => {
+        res.render('home/index')
+    })
     .get('/',async (req, res) => {
         res.render('home/index')
     })
