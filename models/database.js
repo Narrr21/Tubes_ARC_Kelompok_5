82ecdeeb4b7510c7520744eaf026a341324e2575
Nadhif <nadhifalrozin@gmail.com>
2024-04-05 13:41:28 +0700

    fix database blankHost, Nothing found

--- models/database.js
+++ models/database.js
@@ -10,11 +10,11 @@ const pool = mysql.createPool({
     database: process.env.MYSQL_DATABASE
 }).promise()
 
-var blankMakanan = 
+var blankHost = 
 {
-    "kulinerId": "Nothing Found",
-    "foodName": "Nothing Found",
-    "foodOrDrink": "Nothing Found",
+    "hostId": "Nothing Found",
+    "hostName": "Nothing Found",
+    "price": "Nothing Found",
     "images": "Nothing Found",
     "rating": "Nothing Found"
 }
@@ -32,7 +32,7 @@ var blankUser =
 async function check(result, table){
     if (result[0].length === 0) {
         if (table == 'kulinerNangor'){
-            return [blankMakanan]
+            return [blankHost]
         } else if (table == 'dataUser'){
             return [blankUser]
         } else {
@@ -73,11 +73,11 @@ async function getDataBySearch(table, column, value) {
     return check(result, table)
 }
 
-async function searchMakanan(table, columnSearch, price,  columnSort, order, searchInput){
+async function searchMakanan(table, columnSearch, jenis,  columnSort, order, searchInput){
     var result = await pool.query(`
     SELECT * FROM ${table} 
     WHERE ${columnSearch} LIKE '%${searchInput}%'
-    AND price ${price}
+    AND foodOrDrink LIKE '%${jenis}%'
     ORDER BY ${columnSort} ${order};
     `)
     return check(result, table)
@@ -91,79 +91,3 @@ module.exports = {
     getDataMoreLessByCriteria,
     searchMakanan
 }
-
-// async function getMakananById(a){
-//     return await getDataByCriteria('kulinerNangor', 'kulinerId', a)
-// }
-
-// async function getMakananByName(a) {
-//     return await getDataByCriteria('kulinerNangor', 'foodName', a)
-// }
-  
-// async function getMakananByRating(a) {
-//     return await getDataByCriteria('kulinerNangor', 'rating', a)
-// }
-  
-// async function getMakananByType(a) {
-//     return await getDataByCriteria('kulinerNangor', 'foodOrDrink', a)
-// }
-
-// async function getUserByUid(a){
-//     return await getDataByCriteria('dataUser', 'uid', a)
-// }
-
-// async function getUserByName(a){
-//     return await getDataByCriteria('dataUser', 'userName', a)
-// }
-
-// async function getUserByCount(a) {
-//     return await getDataByCriteria('dataUser','countFood', a)
-// }
-
-// async function getMakananMoreByRating(a) {
-//     return await getDataMoreLessByCriteria('kulinerNangor','rating', true ,a);
-// }
-
-// async function getMakananLessByRating(a) {
-//     return await getDataMoreLessByCriteria('kulinerNangor','rating', false ,a);
-// }
-
-// async function getUserMoreByCount(a) {
-//     return await getDataMoreLessByCriteria('dataUser','countFood', true ,a);
-// }
-
-// async function getUserLessByCount(a) {
-//     return await getDataMoreLessByCriteria('dataUser','countFood', false ,a);
-// }
-
-// async function getUserMoreByAge(a) {
-//     return await getDataMoreLessByCriteria('dataUser','userAge', true ,a);
-// }
-
-// async function getUserLessByAge(a) {
-//     return await getDataMoreLessByCriteria('dataUser','userAge', false ,a);
-// }
-
-// async function getUserMoreByUid(a) {
-//     return await getDataMoreLessByCriteria('dataUser','uid', true ,a);
-// }
-
-// async function getUserLessByUid(a) {
-//     return await getDataMoreLessByCriteria('dataUser','uid', false ,a);
-// }
-
-// async function getUserMoreByRating(a) {
-//     return await getDataMoreLessByCriteria('dataUser','rating', true ,a);
-// }
-
-// async function getUserLessByRating(a) {
-//     return await getDataMoreLessByCriteria('dataUser','rating', false ,a);
-// }
-
-// async function getMakananByNameSearch(a) {
-//     return await getDataBySearch('kulinerNangor', 'foodName', a)
-// }
-  
-// async function getMakananByRatingSearch(a) {
-//     return await getDataBySearch('kulinerNangor', 'rating', a)
-// }
